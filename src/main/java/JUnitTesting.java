b9f13af675c89213f3761b1666fb4a56c2a25a86
pranjali khadatkar <khadatkarpranjali1994@gmail.com>
2022-02-28 18:53:30 +0530

    Added: password which contains minimum 8 characters & should have at least 1 upper case

--- src/main/java/JUnitTesting.java
+++ src/main/java/JUnitTesting.java
@@ -3,21 +3,21 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class JUnitTesting {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
+    public static void main (String args[]) {
+        Scanner sc = new Scanner( System.in );
         String password;
-        System.out.println(" enter password which contains minimum 8 characters: ");
+        System.out.println( " enter password which contains minimum 8 characters & should have at least 1 upper case: " );
         password = sc.nextLine();
-        confirmpassword(password);
+        confirmpassword( password );
     }
 
-    public static boolean confirmpassword(String password) {
-        Pattern pattern = Pattern.compile("^[A-Za-z0-9!@#$%^&*()+=?]{8,}$");
-        Matcher matcher = pattern.matcher(password);
+    public static boolean confirmpassword (String password) {
+        Pattern pattern = Pattern.compile( "^(?=.*[A-Z])(?=.*[A-Za-z]).{8,}$" );
+        Matcher matcher = pattern.matcher( password );
         if (matcher.matches()) {
-            System.out.println(" format of password is correct " + password);
+            System.out.println( " format of password is correct " + password );
         } else {
-            System.out.println(" format of password is incorrect.");
+            System.out.println( " format of password is incorrect." );
         }
         return true;
     }
--- src/main/java/MoodAnalyserTest.java
+++ src/main/java/MoodAnalyserTest.java
@@ -5,14 +5,15 @@ public class MoodAnalyserTest {
     JUnitTesting junittesting = new JUnitTesting();
 
     @Test
-    public void  givenPasswordAsPerRule1_Having8Characters_WhenValid_ShouldReturnTrue(){
-        boolean valid = junittesting.confirmpassword("pran@123");
-        Assert.assertEquals(true,valid);
+    public void givenPasswordAsPerRule2_ShouldHaveAtleast1UpperCase_WhenValid_ShouldReturnTrue () {
+        boolean valid = junittesting.confirmpassword( "PRan@123" );
+        Assert.assertEquals( true, valid );
     }
 
     @Test
-    public void  givenPasswordAsPerRule1_Having8Characters_WhenInvalid_ShouldReturnTrue(){
-        boolean valid = junittesting.confirmpassword("pranj>00");
-        Assert.assertEquals(false,valid);
+    public void givenPasswordAsPerRule2_ShouldHaveAtleast1UpperCase_WhenInvalid_ShouldReturnTrue () {
+        boolean valid = junittesting.confirmpassword( "pran>12" );
+        Assert.assertEquals( false, valid );
     }
 }
+

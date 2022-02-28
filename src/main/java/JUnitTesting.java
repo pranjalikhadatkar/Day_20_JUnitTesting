a4a17134694216e92797c9433776757b74a3f1c8
pranjali khadatkar <khadatkarpranjali1994@gmail.com>
2022-02-28 19:12:47 +0530

    Added: password which contains minimum 8 characters & having exactly one special character

--- src/main/java/JUnitTesting.java
+++ src/main/java/JUnitTesting.java
@@ -6,20 +6,20 @@ public class JUnitTesting {
     public static void main (String args[]) {
         Scanner sc = new Scanner( System.in );
         String password;
-        System.out.println( " enter password which contains minimum 8 characters & should have at least 1 numeric number: " );
+        System.out.println( " enter password which contains minimum 8 characters & having exactly one special character: " );
         password = sc.nextLine();
         confirmpassword( password );
     }
 
     public static boolean confirmpassword (String password) {
-        Pattern pattern = Pattern.compile( "^(?=.*[A-Z])(?=.*[0-9])[A-Za-z\\d]{8,}$" );
+        Pattern pattern = Pattern.compile( "^(?=.*[A-Z])(?=.*[0-9])(?=.*[@$!%*#?&])[A-Za-z\\d[@$!%*#?&]]{8,}$" );
         Matcher matcher = pattern.matcher( password );
         if (matcher.matches()) {
             System.out.println( " format of password is correct " + password );
         } else {
             System.out.println( " format of password is incorrect." );
         }
-        return false;
+        return true;
     }
 }
 
--- src/main/java/MoodAnalyserTest.java
+++ src/main/java/MoodAnalyserTest.java
@@ -5,15 +5,14 @@ public class MoodAnalyserTest {
     JUnitTesting junittesting = new JUnitTesting();
 
     @Test
-    public void givenPasswordAsPerRule3_ShouldHaveAtleast1NumericNumber_WhenValid_ShouldReturnTrue () {
+    public void givenPasswordAsPerRule4_ShouldHaveExactlyOneSpecialCharacter_WhenValid_ShouldReturnTrue () {
         boolean valid = junittesting.confirmpassword( "Pran@123" );
         Assert.assertEquals( true, valid );
     }
 
     @Test
-    public void givenPasswordAsPerRule3_ShouldHaveAtleast1NumericNumber_WhenInvalid_ShouldReturnTrue () {
+    public void givenPasswordAsPerRule4_ShouldHaveExactlyOneSpecialCharacter_WhenInvalid_ShouldReturnTrue () {
         boolean valid = junittesting.confirmpassword( "pran>12" );
         Assert.assertEquals( false, valid );
     }
 }
-

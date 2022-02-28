c994aebe0070dd720d41b7530b1b5ab69e5cda61
pranjali khadatkar <khadatkarpranjali1994@gmail.com>
2022-02-28 19:03:13 +0530

    Added: password which contains minimum 8 characters & should have at least 1 numeric number

--- src/main/java/JUnitTesting.java
+++ src/main/java/JUnitTesting.java
@@ -6,20 +6,20 @@ public class JUnitTesting {
     public static void main (String args[]) {
         Scanner sc = new Scanner( System.in );
         String password;
-        System.out.println( " enter password which contains minimum 8 characters & should have at least 1 upper case: " );
+        System.out.println( " enter password which contains minimum 8 characters & should have at least 1 numeric number: " );
         password = sc.nextLine();
         confirmpassword( password );
     }
 
     public static boolean confirmpassword (String password) {
-        Pattern pattern = Pattern.compile( "^(?=.*[A-Z])(?=.*[A-Za-z]).{8,}$" );
+        Pattern pattern = Pattern.compile( "^(?=.*[A-Z])(?=.*[0-9])[A-Za-z\\d]{8,}$" );
         Matcher matcher = pattern.matcher( password );
         if (matcher.matches()) {
             System.out.println( " format of password is correct " + password );
         } else {
             System.out.println( " format of password is incorrect." );
         }
-        return true;
+        return false;
     }
 }
 
--- src/main/java/MoodAnalyserTest.java
+++ src/main/java/MoodAnalyserTest.java
@@ -5,13 +5,13 @@ public class MoodAnalyserTest {
     JUnitTesting junittesting = new JUnitTesting();
 
     @Test
-    public void givenPasswordAsPerRule2_ShouldHaveAtleast1UpperCase_WhenValid_ShouldReturnTrue () {
-        boolean valid = junittesting.confirmpassword( "PRan@123" );
+    public void givenPasswordAsPerRule3_ShouldHaveAtleast1NumericNumber_WhenValid_ShouldReturnTrue () {
+        boolean valid = junittesting.confirmpassword( "Pran@123" );
         Assert.assertEquals( true, valid );
     }
 
     @Test
-    public void givenPasswordAsPerRule2_ShouldHaveAtleast1UpperCase_WhenInvalid_ShouldReturnTrue () {
+    public void givenPasswordAsPerRule3_ShouldHaveAtleast1NumericNumber_WhenInvalid_ShouldReturnTrue () {
         boolean valid = junittesting.confirmpassword( "pran>12" );
         Assert.assertEquals( false, valid );
     }
